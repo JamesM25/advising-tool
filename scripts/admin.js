9424ea48d1f46f4a2367b73bccd551efa4f214f5
James <motherwell.james@student.greenriver.edu>
2024-03-07 13:59:01 -0500

    Begin implementing fixes for delete button icon

--- scripts/admin.js
+++ scripts/admin.js
@@ -56,11 +56,11 @@ function createPrerequisiteRow(courseId) {
     removeBtn.textContent = "Remove";*/
     const removeBtn = row.appendChild(document.createElement("button"));
     removeBtn.className = "remove_button"; // Add the "button" class
-    const svgIcon = document.createElement("svg");
+    const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     svgIcon.setAttribute("viewBox", "0 0 448 512");
-    svgIcon.className = "svgIcon"; // Add the "svgIcon" class
+    svgIcon.classList.add("svgIcon"); // Add the "svgIcon" class
 
-    const path = document.createElement("path");
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path.setAttribute("d", "M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z");
 
     svgIcon.appendChild(path);
